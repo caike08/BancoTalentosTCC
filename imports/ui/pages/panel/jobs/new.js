1f29ea42988d93dc363098c08743d401bbec5bb8
Marcelo Borges <borgespereira.marcelo@gmail.com>
2016-09-15 13:57:25 -0300

    Change 'vacancies' to 'jobs' and organize folders

--- imports/ui/pages/panel/jobs/new.js
+++ imports/ui/pages/panel/jobs/new.js
@@ -1,16 +1,16 @@
 import {
   Template
 } from 'meteor/templating';
-import '/client/html/pages/panel/add-vacancy.html';
-import '/imports/api/collections/vacancies.js';
+import '/client/html/pages/panel/jobs/new.html';
+import '/imports/api/collections/jobs.js';
 
 var json = require('/imports/ui/components/skills.json');
 
-Template.newVacancy.onCreated(function() {
+Template.newJob.onCreated(function() {
   this.skills = new ReactiveVar(json);
 });
 
-Template.newVacancy.onRendered(function() { 
+Template.newJob.onRendered(function() { 
   $("#tags").chosen({
     no_results_text: "Sem resultados para",
     placeholder_text_single: "Selecione uma opção",
@@ -18,7 +18,7 @@ Template.newVacancy.onRendered(function() {
   });
 });
 
-Template.newVacancy.events({
+Template.newJob.events({
   "submit form": function(event) {
     event.preventDefault();
     //REMOVE ERRORS
@@ -35,24 +35,14 @@ Template.newVacancy.events({
       tags: $('#tags').val()
     }
 
-    Meteor.call('saveVacancy', vacancy, function(error) {
+    Meteor.call('saveJob', vacancy, function(error) {
       if (error) {
         Meteor.call('displayErrors', error);
       }
-<<<<<<< 9c9f4d32276565e680d33770c5ee004fbaf662e8
-<<<<<<< 218cf80d1e823c4a06954ce9929bf89a47f8399d
       else {
         toastr.success('Nova oportunidade cadastrada com sucesso', 'Vaga Registrada!');
         resetForm();
       }
-=======
->>>>>>> Add functionality to create vacancies
-=======
-      else {
-        toastr.success('Nova oportunidade cadastrada com sucesso', 'Vaga Registrada!');
-        resetForm();
-      }
->>>>>>> Add simple vacancies listing to student profile
     });
   }
 });
@@ -61,22 +51,13 @@ function targetValue(target) {
   return target.value != "" ? target.value : undefined;
 }
 
-<<<<<<< 9c9f4d32276565e680d33770c5ee004fbaf662e8
-<<<<<<< 218cf80d1e823c4a06954ce9929bf89a47f8399d
-=======
->>>>>>> Add simple vacancies listing to student profile
 function resetForm() {
   $("form")[0].reset();
   $('select option:selected').removeAttr('selected');
   $('select').trigger('chosen:updated');
 }
 
-<<<<<<< 9c9f4d32276565e680d33770c5ee004fbaf662e8
-=======
->>>>>>> Add functionality to create vacancies
-=======
->>>>>>> Add simple vacancies listing to student profile
-Template.newVacancy.helpers({
+Template.newJob.helpers({
   skills: function() {
     return Template.instance().skills.get();
   }
